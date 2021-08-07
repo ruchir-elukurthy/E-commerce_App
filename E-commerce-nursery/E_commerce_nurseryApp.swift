277a759b7e3fac80571d28cc7907065e7360960e
Ruchir <ruchir@pdx.edu>
2021-08-06 19:10:29 -0700

    Initial Commit

---
+++ E-commerce-nursery/E_commerce_nurseryApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerce_nurseryApp.swift
+//  E-commerce-nursery
+//
+//  Created by Ruchir on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct E_commerce_nurseryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
