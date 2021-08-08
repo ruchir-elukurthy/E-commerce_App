2e474ea151b13473b07b60390c6dfcd3970cc619
Ruchir <ruchir@pdx.edu>
2021-08-08 16:33:44 -0700

    login and registration done

--- E-commerce-nursery/App/View/RegisterView.swift
+++ E-commerce-nursery/App/View/RegisterView.swift
@@ -10,50 +10,44 @@ import Firebase
 
 struct RegisterView: View {
     
-    @State var registerSuccess: Bool = false
-    
     @State var email: String = ""
     @State var password: String = ""
     
     var body: some View {
-        NavigationView {
-//            VStack {
-//                Text("Welcome!")
-//                   .font(.largeTitle)
-//                   .fontWeight(.semibold)
-//                Image("registerScreen")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .frame(width: 200, height: 300)
-//                    .clipShape(Circle())
-//                    .cornerRadius(150)
-                VStack {
-                    TextField("Email", text: $email)
+        VStack {
+            Text("Welcome!")
+               .font(.largeTitle)
+               .fontWeight(.semibold)
+            Image("registerScreen")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 200, height: 300)
+                .clipShape(Circle())
+                .cornerRadius(150)
+            TextField("Email", text: $email)
+                .padding()
+                .background(Color(UIColor.lightGray))
+                .cornerRadius(100.0)
+                .padding(.bottom, 20)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(Color(UIColor.lightGray))
+                .cornerRadius(100.0)
+                .padding(.bottom, 20)
+            
+            if register() == true {
+                NavigationLink(destination: ShopView()) {
+                    Text("REGISTER")
+                        .font(.headline)
+                        .foregroundColor(.white)
                         .padding()
-                        .background(Color(UIColor.lightGray))
-                        .cornerRadius(100.0)
-                        .padding(.bottom, 20)
-                    SecureField("Password", text: $password)
-                        .padding()
-                        .background(Color(UIColor.lightGray))
-                        .cornerRadius(100.0)
-                        .padding(.bottom, 20)
-                    Button(action: {
-                        registerSuccess = register()
-                        print(registerSuccess)
-                    }, label: {
-                        Text("REGISTER")
-                            .foregroundColor(.white)
-                            .frame(width: 200, height: 50, alignment: .center)
-                            .cornerRadius(8)
-                            .background(Color.green)
-                    })
-                    if registerSuccess == true {
-                        ShopView()
-                    }
-                }.edgesIgnoringSafeArea(.all)
+                        .frame(width: 220, height: 60)
+                        .background(Color.green)
+                        .cornerRadius(15.0)
+                }
             }
-        }//.edgesIgnoringSafeArea(.all)
+        }.edgesIgnoringSafeArea(.all)
+    }
     
     func register() -> Bool {
         var success: Bool = true
