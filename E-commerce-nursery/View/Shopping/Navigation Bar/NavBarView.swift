6127912f4c59d4ce0dbeb93d6a3d678c651ff300
Ruchir <ruchir@pdx.edu>
2021-10-06 12:01:27 -0700

    cocoaPods implementation completed for tab view title

--- E-commerce-nursery/View/Shopping/Navigation Bar/NavBarView.swift
+++ E-commerce-nursery/View/Shopping/Navigation Bar/NavBarView.swift
@@ -8,6 +8,46 @@
 import SwiftUI
 import LTMorphingLabel
 
+public struct MorphingText: UIViewRepresentable {
+    public typealias UIViewType = LTMorphingLabel
+    
+    var text: String
+    var morphingEffect: LTMorphingEffect
+    var moprhingEnabled: Bool = true
+    var font: UIFont
+    var textColor: UIColor
+    var textAlignment: NSTextAlignment
+    
+    public init(_ text: String,
+                effect: LTMorphingEffect = .scale,
+                font: UIFont = .systemFont(ofSize: 16),
+                textColor: UIColor = .black,
+                textAlignment: NSTextAlignment = .center
+    ) {
+        self.text = text
+        self.morphingEffect = effect
+        self.font = font
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+    }
+
+    public func makeUIView(context: Context) -> UIViewType {
+        let label = LTMorphingLabel(frame: CGRect(origin: .zero, size: CGSize(width: 200, height: 50)))
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        return label
+    }
+
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        uiView.text = text
+        uiView.font = font
+        uiView.morphingEnabled = moprhingEnabled
+        uiView.morphingEffect = morphingEffect
+        uiView.textColor = textColor
+        uiView.textAlignment = textAlignment
+    }
+}
+
 struct NavBarView: View {
     
     @EnvironmentObject var viewModel : RegisterViewModel
@@ -23,11 +63,15 @@ struct NavBarView: View {
                         .padding(.trailing, 60)
             })
             
-            Text("PlantHouse")
-                .font(.title2)
-                .fontWeight(.bold)
-                .padding(.trailing, 20)
-                .foregroundColor(Color(red: 10 / 255, green: 132 / 255, blue: 10 / 255))
+            MorphingText(
+                "PlantHouse",
+                effect: .evaporate,
+                font: UIFont.systemFont(ofSize: 20),
+                textColor: UIColor(red: 10 / 255, green: 132 / 255, blue: 10 / 255, alpha: 0.0),
+                textAlignment: .center
+            )
+            .frame(maxWidth: 200, maxHeight: 100)
+            .padding(.trailing, 20)
             
             Button(action: {
                 viewModel.signOut()
@@ -37,17 +81,6 @@ struct NavBarView: View {
                     .accentColor(.blue)
                     
             }).padding(.trailing, 1)
-//            Button(action: {}, label: {
-//                ZStack {
-//                    Image(systemName: "cart")
-//                        .font(.title)
-//                        .foregroundColor(.black)
-//                    Circle()
-//                        .fill(Color.red)
-//                        .frame(width: 14, height: 14, alignment: .center)
-//                        .offset(x: 13, y: -10)
-//                }
-//            })
         }
     }
 }
