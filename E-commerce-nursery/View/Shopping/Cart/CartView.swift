c629136b2a836dd843e2ec512ce61494bb988106
Ruchir <ruchir@pdx.edu>
2021-10-07 12:53:45 -0700

    products for individual user's cart showing implemented

--- E-commerce-nursery/View/Shopping/Cart/CartView.swift
+++ E-commerce-nursery/View/Shopping/Cart/CartView.swift
@@ -6,35 +6,46 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct CartView: View {
 
     @State private var isEditing:Bool = false
-    
+    @State private var email_id: String = ""
     @ObservedObject private var cartViewModel = CartViewModel()
     
     var body: some View {
         List(cartViewModel.cartProducts) { product in
-            HStack {
-                Image(systemName: "person.fill")
-                    .data(url: URL(string: product.image_link)!)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 150, height: 100, alignment: .center)
-                    .cornerRadius(15)
-                    .padding(5)
-                VStack {
-                    Text(product.name)
-                        .padding(.bottom,10)
-                    Text("$"+String(product.price))
-                        .bold()
+            if product.user == email_id {
+                HStack {
+                    Image(systemName: "person.fill")
+                        .data(url: URL(string: product.image_link)!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 100, alignment: .center)
+                        .cornerRadius(15)
+                        .padding(5)
+                    VStack {
+                        Text(product.name)
+                            .padding(.bottom,10)
+                        Text("$"+String(product.price))
+                            .bold()
+                    }
+                    .padding(.trailing,40)
+                    .padding(.bottom,10)
                 }
-                .padding(.trailing,40)
-                .padding(.bottom,10)
             }
         }
         .onAppear() {
             self.cartViewModel.fetchData()
+            self.getCurrentUser()
+        }
+    }
+    
+    func getCurrentUser() {
+        let user = Auth.auth().currentUser
+        if let user = user {
+            self.email_id = user.email ?? ""
         }
     }
 }
